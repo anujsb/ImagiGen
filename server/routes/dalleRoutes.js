1d781bb7de64baaae11662b2058949bd48ca8aa4
anujsb <anujsbhuyar@gmail.com>
2023-10-29 14:34:36 +0530

    changes

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import Configuration from 'openai';
+import OpenAIApi from 'openai';
+import cors from 'cors';
+
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+router.use(cors()); // Enable CORS for your router
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: 'Hello from DALL-E!' });
+});
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error?.response?.data?.error?.message || 'Something went wrong');
+  }
+});
+
+export default router;
